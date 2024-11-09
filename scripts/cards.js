a96c3db6d2b79b969063a86a4e3d22b270447c77
Usachev Leonid <leonidusachev04@yandex.ru>
2024-11-09 14:45:36 +0300

    cards.js is ready

--- scripts/cards.js
+++ scripts/cards.js
@@ -24,3 +24,19 @@ const initialCards = [
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
     }
 ];
+
+
+let placesList = document.querySelector('.places__list')
+
+function createCard(cardName, cardLink) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  cardElement.querySelector('.card__image').src = cardLink;
+  cardElement.querySelector('.card__image').alt = cardName;
+  cardElement.querySelector('.card__title').textContent = cardName;
+  return cardElement;
+}
+
+initialCards.forEach(function (item) {
+  placesList.append(createCard(item.name, item.link));
+});
